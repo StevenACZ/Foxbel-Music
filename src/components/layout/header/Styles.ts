6861f1a97a213de51ba3d5ad58a22e120a7b948e
stevenacz <scoailazaa@gmail.com>
2021-04-26 15:17:38 -0500

    grid-area added

--- src/components/layout/header/Styles.ts
+++ src/components/layout/header/Styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const HeaderStyled = styled.header`
+  grid-area: header;
+  border: 1px solid red;
+
   @media (max-width: 991px) {
     padding: 30px 20px;
   }
