c44ae883a7f1ffd0b57fd77e4cce6d950a7e4ebc
stevenacz <scoailazaa@gmail.com>
2021-04-26 16:13:57 -0500

    structure improved

--- src/components/layout/aside/Aside.tsx
+++ src/components/layout/aside/Aside.tsx
@@ -2,12 +2,50 @@
 import React from 'react';
 
 // Styles
-import { AsideStyled } from './Styles';
+import {
+  AsideStyled,
+  Logo,
+  Navigation,
+  List,
+  OptionList,
+  OptionListItem,
+} from './Styles';
+
+// Images
+import logo from '../../../assets/images/logo-foxbel-music.png';
 
 interface Props {}
 
 const Aside: React.FC<Props> = () => {
-  return <AsideStyled>Aside</AsideStyled>;
+  return (
+    <AsideStyled>
+      <Logo>
+        <img src={logo} alt="Logo" />
+      </Logo>
+      <Navigation>
+        <List>
+          <h3>Mi Librería</h3>
+          <OptionList>
+            <OptionListItem className="active">Recientes</OptionListItem>
+            <OptionListItem>Artistaas</OptionListItem>
+            <OptionListItem>Álbumns</OptionListItem>
+            <OptionListItem>Canciones</OptionListItem>
+            <OptionListItem>Estaciones</OptionListItem>
+          </OptionList>
+        </List>
+
+        <List>
+          <h3>Playlist</h3>
+          <OptionList>
+            <OptionListItem>Metal</OptionListItem>
+            <OptionListItem>Para bailar</OptionListItem>
+            <OptionListItem>Rock 90s</OptionListItem>
+            <OptionListItem>Baladas</OptionListItem>
+          </OptionList>
+        </List>
+      </Navigation>
+    </AsideStyled>
+  );
 };
 
 export default Aside;
