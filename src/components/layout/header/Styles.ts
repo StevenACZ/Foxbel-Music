723e22274f0ae5ebeeec779e0ac11dd9dc47d90a
stevenacz <scoailazaa@gmail.com>
2021-04-26 13:41:43 -0500

    added

---
+++ src/components/layout/header/Styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const HeaderStyled = styled.header`
+  @media (max-width: 991px) {
+    padding: 30px 20px;
+  }
+
+  @media (max-width: 767px) {
+  }
+`;
