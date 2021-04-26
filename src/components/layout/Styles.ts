1bc27d5047b3e1370b64c9295737ae065f29ad5c
stevenacz <scoailazaa@gmail.com>
2021-04-26 13:31:50 -0500

    improved

---
+++ src/components/layout/Styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  max-width: 992px;
+  padding: 30px 0;
+  margin: 0 auto;
+
+  @media (max-width: 991px) {
+    padding: 30px 20px;
+  }
+
+  @media (max-width: 767px) {
+  }
+`;
