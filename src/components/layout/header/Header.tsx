0b42800760076d34b376ddbcc80cedf0d62f16cf
stevenacz <scoailazaa@gmail.com>
2021-04-26 16:38:27 -0500

    Header added

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -2,12 +2,26 @@
 import React from 'react';
 
 // Styles
-import { HeaderStyled } from './Styles';
+import { HeaderStyled, Search, User } from './Styles';
+
+// Ant Icons
+import { UserOutlined, SearchOutlined } from '@ant-design/icons';
 
 interface Props {}
 
 const Header: React.FC<Props> = () => {
-  return <HeaderStyled>Header</HeaderStyled>;
+  return (
+    <HeaderStyled>
+      <Search>
+        <input type="text" placeholder="Buscar" />
+        <SearchOutlined />
+      </Search>
+      <User>
+        <UserOutlined />
+        <span>Francisco M.</span>
+      </User>
+    </HeaderStyled>
+  );
 };
 
 export default Header;
