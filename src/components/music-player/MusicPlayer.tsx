c4438c073556de204ce8ffa0ae7aa2d844e07bea
stevenacz <scoailazaa@gmail.com>
2021-04-26 15:18:16 -0500

    added

---
+++ src/components/music-player/MusicPlayer.tsx
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+
+// Styles
+import { MusicPlayerStyled } from './Styles';
+
+interface Props {}
+
+const MusicPlayer: React.FC<Props> = () => {
+  return <MusicPlayerStyled>MusicPlayer</MusicPlayerStyled>;
+};
+
+export default MusicPlayer;
