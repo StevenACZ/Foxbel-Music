724756737ff1712d0b339bbbe2af51cc3bb424e7
stevenacz <scoailazaa@gmail.com>
2021-04-26 13:41:08 -0500

    aside added

---
+++ src/components/layout/aside/Aside.tsx
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+
+// Styles
+import { AsideStyled } from './Styles';
+
+interface Props {}
+
+const Aside: React.FC<Props> = () => {
+  return <AsideStyled>Aside</AsideStyled>;
+};
+
+export default Aside;
