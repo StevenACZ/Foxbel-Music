bc3a4ac54642ffc5f3b38bf29503707d3167b7fa
stevenacz <scoailazaa@gmail.com>
2021-04-26 13:41:18 -0500

    added

---
+++ src/components/layout/aside/Styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const AsideStyled = styled.aside`
+  @media (max-width: 991px) {
+    padding: 30px 20px;
+  }
+
+  @media (max-width: 767px) {
+  }
+`;
