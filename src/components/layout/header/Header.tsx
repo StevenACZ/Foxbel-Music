39aa4ad901f5478d3d7534029eba973036b79dd7
stevenacz <scoailazaa@gmail.com>
2021-04-26 13:41:34 -0500

    Header added

---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+
+// Styles
+import { HeaderStyled } from './Styles';
+
+interface Props {}
+
+const Header: React.FC<Props> = () => {
+  return <HeaderStyled>Header</HeaderStyled>;
+};
+
+export default Header;
