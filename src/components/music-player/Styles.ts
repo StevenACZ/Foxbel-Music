98bb3a245a15d5956bf8b56740a028c41743e707
stevenacz <scoailazaa@gmail.com>
2021-04-26 15:18:22 -0500

    added

---
+++ src/components/music-player/Styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const MusicPlayerStyled = styled.aside`
+  grid-area: musicPlayer;
+  border: 1px solid purple;
+
+  @media (max-width: 991px) {
+    padding: 30px 20px;
+  }
+
+  @media (max-width: 767px) {
+  }
+`;
