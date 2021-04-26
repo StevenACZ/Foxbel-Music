234add78bc74f7a6b0a23fd3171ac601067ef8d6
stevenacz <scoailazaa@gmail.com>
2021-04-26 13:23:25 -0500

    added

---
+++ src/theme/variables.ts
@@ -0,0 +1,19 @@
+export const color = {
+  /* Red Principal */
+  colorRedPrincipal: '#E86060',
+
+  /* Dark Red Secondary */
+  colorDarkRedSecondary: '#662323',
+
+  /* Light-red */
+  colorLightRed: '#FF7676',
+
+  /* Gray */
+  colorGray: '#828282',
+
+  /* Soft gray */
+  colorSoftGray: '#BDBDBD',
+
+  colorWhite: '#fff',
+  colorBlack: '#000',
+};
