92af99528a2c460069c8f2557627b6e323832798
stevenacz <scoailazaa@gmail.com>
2021-04-27 11:42:21 -0500

    improved

--- src/components/layout/header/Styles.ts
+++ src/components/layout/header/Styles.ts
@@ -7,45 +7,6 @@ export const HeaderStyled = styled.header`
   display: flex;
   justify-content: space-between;
   align-content: center;
-
-  @media (max-width: 991px) {
-    padding: 30px 20px;
-  }
-
-  @media (max-width: 767px) {
-  }
-`;
-
-export const Search = styled.label`
-  display: flex;
-  position: relative;
-  width: 59%;
-  border-radius: 100px;
-  height: 36px;
-
-  & > input {
-    border: 1px solid ${color.colorGray};
-    outline: none;
-    width: 100%;
-    border-radius: 100px;
-    padding-left: 16px;
-
-    ::placeholder {
-      color: ${color.colorSoftGray};
-      font-size: 18px;
-      line-height: 22.5px;
-      font-weight: 400;
-    }
-  }
-
-  & > .anticon-search {
-    position: absolute;
-    top: 50%;
-    right: 10px;
-    transform: translateY(-50%);
-    color: ${color.colorSoftGray};
-    cursor: pointer;
-  }
 `;
 
 export const User = styled.div`
