bd4264d4b8b33b80ff653d0fafe0dd5de0a64d8c
stevenacz <scoailazaa@gmail.com>
2021-04-26 20:26:06 -0500

    improved

---
+++ src/components/music-list/Styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { color } from '../../theme/variables';
+
+export const MusicListStyled = styled.section`
+  margin-top: 40px;
+
+  & > h2 {
+    font-family: 'Quicksand', sans-serif;
+    color: ${color.colorRedPrincipal};
+    font-size: 22px;
+    line-height: 27.5px;
+    font-weight: 700;
+  }
+`;
+
+export const MusicListContent = styled.ul`
+  margin-top: 20px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+  grid-auto-rows: auto;
+  grid-column-gap: 22px;
+  grid-row-gap: 25px;
+`;
