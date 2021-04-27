403bb1d41651e976de4e04d76f37ef334e71266a
stevenacz <scoailazaa@gmail.com>
2021-04-27 12:18:59 -0500

    data from redux added

--- src/components/music-list/MusicList.tsx
+++ src/components/music-list/MusicList.tsx
@@ -1,5 +1,16 @@
 // React
-import React from 'react';
+import React, { useEffect } from 'react';
+
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+
+// Redux - Slices
+import {
+  selectSongListSongs,
+  selectSongListLoading,
+  selectSongListError,
+  songListReset,
+} from '../../slices/song/songList';
 
 // Styles
 import { MusicListStyled, MusicListContent } from './Styles';
@@ -10,21 +21,29 @@ import MusicListItem from './music-list-item/MusicListItem';
 interface Props {}
 
 const MusicList: React.FC<Props> = () => {
+  // Dispatch
+  const dispatch = useDispatch();
+
+  // Selector
+  const songs = useSelector(selectSongListSongs);
+  const loading = useSelector(selectSongListLoading);
+  const error = useSelector(selectSongListError);
+
+  useEffect(() => {
+    return () => {
+      dispatch(songListReset());
+    };
+  }, [dispatch]);
+
   return (
     <MusicListStyled>
       <h2>Resultados</h2>
 
       <MusicListContent>
-        <MusicListItem />
-        <MusicListItem />
-        <MusicListItem />
-        <MusicListItem />
-        <MusicListItem />
-        <MusicListItem />
-        <MusicListItem />
-        <MusicListItem />
-        <MusicListItem />
-        <MusicListItem />
+        {songs &&
+          songs.map((song: any) => <MusicListItem key={song.id} {...song} />)}
+
+        {/* {error && <Alert message={error} type="error" />} */}
       </MusicListContent>
     </MusicListStyled>
   );
