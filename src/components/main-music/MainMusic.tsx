64af53d4f11ee8ca1345ebb0d75d21833081dc26
stevenacz <scoailazaa@gmail.com>
2021-04-27 18:30:35 -0500

    imporoved

--- src/components/main-music/MainMusic.tsx
+++ src/components/main-music/MainMusic.tsx
@@ -53,7 +53,7 @@ const MainMusic: React.FC<Props> = () => {
 
   return (
     <>
-      {songs &&
+      {songs ? (
         songs
           .slice(0, 1)
           .map(
@@ -98,7 +98,10 @@ const MainMusic: React.FC<Props> = () => {
                 </More>
               </MainMusicStyled>
             )
-          )}
+          )
+      ) : (
+        <Alert message="Busque algo..." type="warning" />
+      )}
       {loading && <Spin spinning={loading} />}
       {error && <Alert message={error} type="error" />}
     </>
