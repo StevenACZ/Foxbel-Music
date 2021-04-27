7433577778c37c7542d77017cb82d673f34c552b
stevenacz <scoailazaa@gmail.com>
2021-04-26 21:41:51 -0500

    improved

--- src/components/music-player/MusicPlayer.tsx
+++ src/components/music-player/MusicPlayer.tsx
@@ -2,12 +2,51 @@
 import React from 'react';
 
 // Styles
-import { MusicPlayerStyled } from './Styles';
+import {
+  MusicPlayerStyled,
+  Image,
+  Player,
+  Infomation,
+  Actions,
+  Volume,
+} from './Styles';
+
+// Ant Icons
+import {
+  PlayCircleOutlined,
+  PauseCircleOutlined,
+  StepBackwardOutlined,
+  StepForwardOutlined,
+} from '@ant-design/icons';
+
+// Images
+import example from '../../assets/images/adele21.png';
 
 interface Props {}
 
 const MusicPlayer: React.FC<Props> = () => {
-  return <MusicPlayerStyled>MusicPlayer</MusicPlayerStyled>;
+  return (
+    <MusicPlayerStyled>
+      <Image>
+        <img src={example} alt="example" />
+      </Image>
+      <Player>
+        <Infomation>
+          <h3>Canción</h3>
+          <p>Artista - Álbum</p>
+        </Infomation>
+
+        <Actions>
+          <StepBackwardOutlined />
+          <PlayCircleOutlined />
+          <PauseCircleOutlined />
+          <StepForwardOutlined />
+        </Actions>
+
+        <Volume>2</Volume>
+      </Player>
+    </MusicPlayerStyled>
+  );
 };
 
 export default MusicPlayer;
