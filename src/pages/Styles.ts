79b3f3236317584273fff0730df688de861c9c56
stevenacz <scoailazaa@gmail.com>
2021-04-26 19:49:24 -0500

    improved

--- src/pages/Styles.ts
+++ src/pages/Styles.ts
@@ -1,19 +1,5 @@
 import styled from 'styled-components';
 
-export const HomeScreenStyled = styled.section``;
-
-export const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-
-  & > h2 {
-    text-transform: uppercase;
-  }
-
-  & > button {
-    width: auto;
-    padding: 0 20px;
-  }
+export const RecentScreenStyled = styled.main`
+  padding: 15px 30px;
 `;
