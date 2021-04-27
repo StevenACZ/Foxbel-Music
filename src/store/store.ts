4014ec621fee0312abf4d5df285b7eb7aa1c03b2
stevenacz <scoailazaa@gmail.com>
2021-04-27 16:57:02 -0500

    song details added

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,11 +3,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 // Redux - Slices
 import { songListSlice } from '../slices/song/songList';
+import { songDetailsSlice } from '../slices/song/songDetails';
 
 const store = configureStore({
   reducer: {
     songList: songListSlice.reducer,
-    // songDetails: songDetailsSlice.reducer,
+    songDetails: songDetailsSlice.reducer,
   },
 });
 
